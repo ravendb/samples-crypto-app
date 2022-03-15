7593db7cabf4f218a664b2855e83cd50680abf38
Grzegorz Lachowski <gregolsky@ravendb.net>
2022-03-15 10:29:14 +0100

    Load certificate by thumbprint in Azure functions Add 2 scripts to package.json Update frontend packages

--- functions/backend-dotnet/Program.cs
+++ functions/backend-dotnet/Program.cs
@@ -13,54 +13,78 @@ using Raven.Demos.CryptoApp.Models;
 
 namespace Raven.Demos.CryptoApp
 {
-  public class Program
-  {
-    public static void Main()
-    {
-      var host = new HostBuilder()
-          .ConfigureFunctionsWorkerDefaults(builder =>
-          {
-            builder.Services.Configure<JsonSerializerOptions>(jsonSerializerOptions =>
-            {
-              // Camel-case property name policy
-              jsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
-            });
-          })
-          .ConfigureServices(services =>
-          {
-            services.TryAddSingleton<DocumentStore>(provider =>
-            {
-              var store = new DocumentStore()
-              {
-                Urls = new string[] {
-                  GetEnvironmentVariable("DB_URL")
-                },
-                Database = GetEnvironmentVariable("DB_NAME"),
-                Certificate = new System.Security.Cryptography.X509Certificates.X509Certificate2(
-                  GetEnvironmentVariable("DB_CERT_PATH"),
-                  GetEnvironmentVariable("DB_PASSWORD"),
-                  X509KeyStorageFlags.MachineKeySet),
-              };
-              store.Initialize();
-              store.TimeSeries.Register<MarketSymbol, SymbolPrice>("history");
-              return store;
+	public class Program
+	{
+		public static void Main()
+		{
+			var host = new HostBuilder()
+			    .ConfigureFunctionsWorkerDefaults(builder =>
+			    {
+				    builder.Services.Configure<JsonSerializerOptions>(jsonSerializerOptions =>
+		  {
+		    // Camel-case property name policy
+		    jsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+			      });
+			    })
+			    .ConfigureServices(services =>
+			    {
+				    services.TryAddSingleton<DocumentStore>(provider =>
+		  {
+				      var certificate = GetRavendbCertificate();
+				      var url = GetEnvironmentVariable("DB_URL");
+				      var db = GetEnvironmentVariable("DB_NAME");
+				      var store = new DocumentStore()
+				      {
+					      Urls = new string[] { url },
+					      Database = db,
+					      Certificate = certificate
+				      };
+				      store.Initialize();
+				      store.TimeSeries.Register<MarketSymbol, SymbolPrice>("history");
+				      return store;
 
-            });
+			      });
 
-            services.TryAddTransient<IAsyncDocumentSession>(provider =>
-            {
-              var store = provider.GetService(typeof(DocumentStore)) as DocumentStore;
-              return store.OpenAsyncSession();
-            });
-          })
-          .Build();
+				    services.TryAddTransient<IAsyncDocumentSession>(provider =>
+		  {
+				      var store = provider.GetService(typeof(DocumentStore)) as DocumentStore;
+				      return store.OpenAsyncSession();
+			      });
+			    })
+			    .Build();
 
-      host.Run();
-    }
+			host.Run();
+		}
 
-    private static string GetEnvironmentVariable(string name)
-    {
-      return System.Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Process);
-    }
-  }
+		private static X509Certificate2 GetRavendbCertificate()
+		{
+			var certThumb = Environment.GetEnvironmentVariable("DB_CERT_THUMBPRINT");
+
+			X509Store certStore = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+			certStore.Open(OpenFlags.ReadOnly);
+
+			X509Certificate2Collection certCollection = certStore.Certificates
+			    .Find(X509FindType.FindByThumbprint, certThumb, false);
+
+			// Get the first cert with the thumbprint
+			if (certCollection.Count > 0)
+			{
+				X509Certificate2 cert = certCollection[0];
+				return cert;
+			}
+
+			certStore.Close();
+
+			throw new Exception($"Certificate {certThumb} not found.");
+		}
+
+		private static string GetEnvironmentVariable(string name)
+		{
+			var val = System.Environment.GetEnvironmentVariable(name);
+			if (string.IsNullOrEmpty(val))
+				throw new ArgumentException($"Environment variable '{name}' is required.");
+			return val;
+		}
+
+	}
 }
--- functions/ingestion-dotnet/Program.cs
+++ functions/ingestion-dotnet/Program.cs
@@ -6,63 +6,93 @@ using Raven.Client.Documents.Session;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using System;
 using System.Security.Cryptography.X509Certificates;
+using Microsoft.Extensions.Configuration;
 using Raven.Demos.CryptoApp.Models;
 using Refit;
 
 namespace Raven.Demos.CryptoApp
 {
-  public class Program
-  {
-    public static void Main()
+    public class Program
     {
-      var host = new HostBuilder()
-          .ConfigureFunctionsWorkerDefaults(builder =>
-          {
-            builder.Services.Configure<JsonSerializerOptions>(jsonSerializerOptions =>
+        public static void Main()
+        {
+            var host = new HostBuilder()
+                .ConfigureAppConfiguration(configBuilder => configBuilder.AddEnvironmentVariables())
+                .ConfigureFunctionsWorkerDefaults(builder =>
+                {
+                    builder.Services.Configure<JsonSerializerOptions>(jsonSerializerOptions =>
+                    {
+                        // Camel-case property name policy
+                        jsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+                    });
+                })
+                .ConfigureServices(services =>
+                {
+                    services.TryAddSingleton<DocumentStore>(provider =>
+                    {
+                        var certBytesBase64 = GetEnvironmentVariable("DB_CERT_BASE64");
+                        var certificateBytes = Convert.FromBase64String(certBytesBase64);
+                        var store = new DocumentStore()
+                        {
+                            Urls = new string[]
+                            {
+                                GetEnvironmentVariable("DB_URL")
+                            },
+                            Database = GetEnvironmentVariable("DB_NAME"),
+                            Certificate = GetRavendbCertificate()
+                        };
+
+                        store.Initialize();
+                        store.TimeSeries.Register<MarketSymbol, SymbolPrice>("history");
+                        return store;
+                    });
+
+                    services.TryAddTransient<IAsyncDocumentSession>(provider =>
+                    {
+                        var store = provider.GetService(typeof(DocumentStore)) as DocumentStore;
+                        return store.OpenAsyncSession();
+                    });
+
+                    services
+                        .AddRefitClient<IKucoinApi>()
+                        .ConfigureHttpClient(c => c.BaseAddress = new Uri("https://openapi-v2.kucoin.com"));
+                })
+                .Build();
+
+            host.Run();
+        }
+
+        private static X509Certificate2 GetRavendbCertificate()
+        {
+            var certThumb = GetEnvironmentVariable("DB_CERT_THUMBPRINT");
+            
+            X509Store certStore = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+            certStore.Open(OpenFlags.ReadOnly);
+            
+            X509Certificate2Collection certCollection = certStore.Certificates
+                .Find(X509FindType.FindByThumbprint, certThumb, false);
+            
+            // Get the first cert with the thumbprint
+            if (certCollection.Count > 0)
             {
-              // Camel-case property name policy
-              jsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
-            });
-          })
-          .ConfigureServices(services =>
-          {
-            services.TryAddSingleton<DocumentStore>(provider =>
+                X509Certificate2 cert = certCollection[0];
+                return cert;
+            }
+
+            certStore.Close();
+
+            throw new Exception($"Certificate {certThumb} not found.");
+        }
+
+        private static string GetEnvironmentVariable(string name)
+        {
+            var val = System.Environment.GetEnvironmentVariable(name);
+            if (string.IsNullOrEmpty(val))
             {
-              var store = new DocumentStore()
-              {
-                Urls = new string[] {
-                  GetEnvironmentVariable("DB_URL")
-                },
-                Database = GetEnvironmentVariable("DB_NAME"),
-                Certificate = new System.Security.Cryptography.X509Certificates.X509Certificate2(
-                  GetEnvironmentVariable("DB_CERT_PATH"),
-                  GetEnvironmentVariable("DB_PASSWORD"),
-                  X509KeyStorageFlags.MachineKeySet),
-              };
-              store.Initialize();
-              store.TimeSeries.Register<MarketSymbol, SymbolPrice>("history");
-              return store;
+                throw new ArgumentException($"Env var {name} was not passed.");
+            }
 
-            });
-
-            services.TryAddTransient<IAsyncDocumentSession>(provider =>
-            {
-              var store = provider.GetService(typeof(DocumentStore)) as DocumentStore;
-              return store.OpenAsyncSession();
-            });
-
-            services
-              .AddRefitClient<IKucoinApi>()
-              .ConfigureHttpClient(c => c.BaseAddress = new Uri("https://openapi-v2.kucoin.com"));
-          })
-          .Build();
-
-      host.Run();
+            return val;
+        }
     }
-
-    private static string GetEnvironmentVariable(string name)
-    {
-      return System.Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Process);
-    }
-  }
 }

f407b38f636064a17ad7b4c21a739e64e34fd8ea
Grzegorz Lachowski <gregolsky@ravendb.net>
2022-03-16 13:00:11 +0100

    code formatting

--- functions/ingestion-dotnet/Program.cs
+++ functions/ingestion-dotnet/Program.cs
@@ -65,13 +65,13 @@ namespace Raven.Demos.CryptoApp
         private static X509Certificate2 GetRavendbCertificate()
         {
             var certThumb = GetEnvironmentVariable("DB_CERT_THUMBPRINT");
-            
+
             X509Store certStore = new X509Store(StoreName.My, StoreLocation.CurrentUser);
             certStore.Open(OpenFlags.ReadOnly);
-            
+
             X509Certificate2Collection certCollection = certStore.Certificates
                 .Find(X509FindType.FindByThumbprint, certThumb, false);
-            
+
             // Get the first cert with the thumbprint
             if (certCollection.Count > 0)
             {
